cf8c75dedd043fba4c90b5212da9c47c7c1ef6dc
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-01-01 05:25:08 +0000

    remove alert

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let rock = document.getElementById("rock")
 let paper = document.getElementById("paper")
 let scissors = document.getElementById("scissors")
 
-rock.onclick = () => console.log("user played rock")
-paper.onclick = () => console.log("user played paper")
-scissors.onclick = () => console.log("user played scissors")
+rock.onclick = () => console.log("user played Rock")
+paper.onclick = () => console.log("user played Paper")
+scissors.onclick = () => console.log("user played Scissors")
+reset.onclick = () => console.log("user Reset the game")
 
-alert("Hello")
+alert("Greetings player. Welcome to Rock Paper Scissors, click the buttons displayed below to . The Rules ar simple. Rock > Scissor > Paper...")
