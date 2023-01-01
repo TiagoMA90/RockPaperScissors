ba425b449daaade7e56e9ad881c5506babdb9383
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-01-01 06:02:09 +0000

    add test to reset button

--- script.js
+++ script.js
@@ -6,5 +6,3 @@ rock.onclick = () => console.log("user played Rock")
 paper.onclick = () => console.log("user played Paper")
 scissors.onclick = () => console.log("user played Scissors")
 reset.onclick = () => console.log("user Reset the game")
-
-alert("Greetings player. Welcome to Rock Paper Scissors, click the buttons displayed below to . The Rules ar simple. Rock > Scissor > Paper...")
